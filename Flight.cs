52df6e0a1f00de5c98db56f67d0101d5c3fac3b8
Alluru Rishitha Reddy <s10268022@connect.np.edu.sg>
2025-01-20 12:33:56 +0800

    Added namespace for classes and imported namespace into program.cs

--- Flight.cs
+++ Flight.cs
@@ -1,1 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
+namespace PRG2_Assignment
+{
+    class Flight
+    {
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,1 +1,4 @@
-
+// See https://aka.ms/new-console-template for more information
+using PRG2_Assignment;
+using System.Runtime.CompilerServices;
+using System.Text;
