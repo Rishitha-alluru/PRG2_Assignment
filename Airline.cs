947d4475d070cfa78d018e83b4769f28961e62dc
Yi Fei Faye Cheah <s10269175@connect.np.edu.sg>
2025-01-25 20:43:17 +0800

    Added Airline class

--- Airline.cs
+++ Airline.cs
@@ -9,6 +9,42 @@ namespace PRG2_Assignment
 {
     class Airline
     {
+        public string Name { get; set; }
+        public string Code { get; set; }
+        public Dictionary<string, Flight> Flights { get; private set; } = new Dictionary<string, Flight>();
 
+        public Airline(string name, string code)
+        {
+            Name = name;
+            Code = code;
+        }
+        public bool AddFlight(Flight flight)
+        {
+            if (!Flights.ContainsKey(flight.FlightNumber))
+            {
+                Flights[flight.FlightNumber] = flight;
+                return true;
+            }
+            return false;
+        }
+
+        public bool RemoveFlight(Flight flight)
+        {
+            return Flights.Remove(flight.FlightNumber);
+        }
+
+        public double CalculateFees()
+        {
+            double totalFees = 0.0;
+            foreach (KeyValuePair<string, Flight> flight in Flights)
+            {
+                totalFees += flight.Value.CalculateFees();
+            }
+            return totalFees;
+        }
+
+        public override string ToString()
+        {
+            return "Airline: " + Name + " Code: " + Code;
+        }
     }
-}
